c78ac576474a65c48342a476a1443d21c860f992
jssimbine <jennyfersimbine@gmail.com>
2022-12-09 07:57:33 -0800

    modified files

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+	
+#include <stdlib.h>
+
+
+
+/**                                                                                                                                     
+
+ * main - Entry point                                                                                                                   
+
+ *                                                                                                                                      
+
+ *Return: Always 0 (Success)                                                                                                            
+
+ */
+
+
+
+int main(void)
+	
+{
+	
+        char alphab;
+	
+
+	
+        alphab = 'a';
+	
+        while (alphab <= 'z')
+		
+        {
+		
+                putchar(alphab);
+		
+                alphab = alphab + 1;
+		
+        }
+	
+        putchar('\n');
+	
+        return (0);
+	
+}#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+ /**                                                                                                                                     
+
+  * main - Entry point                                                                                                                   
+
+  *                                                                                                                                      
+
+  *Return: Always 0 (Success)                                                                                                            
+
+  */
+
+
+
+int main(void)
+
+{
+	
+        char alphab;
+	
+
+	
+        alphab = 'a';
+	
+        while (alphab <= 'z')
+		
+        {
+		
+                putchar(alphab);
+		
+                alphab = alphab + 1;
+		
+        }
+	
+        putchar('\n');
+	
+        return (0);
+	
+}#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+ /**                                                                                                                                     
+
+  * main - Entry point                                                                                                                   
+
+  *                                                                                                                                      
+
+  *Return: Always 0 (Success)                                                                                                            
+
+  */
+
+
+
+int main(void)
+
+{
+	
+        char alphab;
+	
+
+	
+        alphab = 'a';
+	
+        while (alphab <= 'z')
+		
+        {
+		
+                putchar(alphab);
+		
+                alphab = alphab + 1;
+		
+        }
+	
+        putchar('\n');
+	
+        return (0);
+	
+}<<kjk
