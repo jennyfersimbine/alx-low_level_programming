c8ee7c0c151e2700b3e394ca80eedb5e7452a0f5
jssimbine <jennyfersimbine@gmail.com>
2022-12-09 15:33:06 -0800

    added file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always return 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 96; i < 122; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
