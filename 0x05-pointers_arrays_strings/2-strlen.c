252e450af2c2775b7924a2051cf33e6729ae1813
jssimbine <jennyfersimbine@gmail.com>
2022-12-20 01:52:08 -0800

    file task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * _strlen - return length of a string
+ * @s: pointer paramenter
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+	length = sizeof(*s);
+	return length;
+}
