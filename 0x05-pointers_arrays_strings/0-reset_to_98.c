2c31642b2630ef48aedd7b68170f0a0215a71126
jssimbine <jennyfersimbine@gmail.com>
2022-12-20 01:17:51 -0800

    fixed initial files

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,13 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * reset_to_98 -  takes a pointer to an int as parameter and update value to 98
  * @n: parameter to function
- * Return: 0
+ * Return: none
  */
 
 void reset_to_98(int *n)
 {
-	int c;
-	
-	c = *n;
-	_putchar(c);
 	*n = 98;
-
-	_putchar(c);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include "_putchar.c"
-
 /**
  * main - run functions
  * _putchar - print rint functions
@@ -8,10 +5,21 @@
  *
  * reset_to_98 - task 1
  * @n: parameter
- *
+ * 
+ * swap_int - task 2
+ * @a: parmtr 1
+ *@b: prmter 2;
  * Return: always 0
  */
 
-int _putchar (char c);
+int _putchar(char c);
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
