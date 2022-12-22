5c924fd8947395080f79149d9c86165fbabcad97
jssimbine <jennyfersimbine@gmail.com>
2022-12-22 11:53:15 -0800

    file task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int ans;
+
+	ans = strcmp(s1, s2);
+	return ans;
+}
