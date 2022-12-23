c64c4e442a1109d5ad70d4ea07ec48f09d4f9f01
jssimbine <jennyfersimbine@gmail.com>
2022-12-23 15:19:04 -0800

    file task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * cap_string - capitalize all words
+ * @s: string with words
+ * Return: string modified
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i == 0)
+		{
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] -= 32;
+			}
+			continue;
+		}
+		if (s[i] == ' ')
+		{
+			++i;
+			if (s[i] >= 97 && s[i] <= 122)
+			{
+				s[i] -= 32;
+				continue;
+			}
+		} else
+		{
+			if (s[i] >= 65 && s[i] <= 97)
+			{
+				s[i] += 32;
+			}
+		}
+	}
+	return (s);
+}
