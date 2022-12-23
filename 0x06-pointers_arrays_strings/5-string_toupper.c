8467ba076c9a3305356915eea987a4dfed0a7eb0
jssimbine <jennyfersimbine@gmail.com>
2022-12-23 13:14:24 -0800

    task5 fixed

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,12 +11,13 @@
 
 char *string_toupper(char *c)
 {
-	int i; 
-	for (i = 0; c[i] != '\0'; i++)
+	int i;
+	
+	for (i = 0; c[i]; i++)
 	{
-		if (c[i] >= 'a' && c[i] <= 'z')
+		if (c[i] >= 97 && c[i] <= 122)
 		{
-			c[i] = c[i] -32;
+			c[i] -= 32;
 		}
 	}
 	return (0);
