c4918d6c4b643135fc99e4b81b9fe56bf5de3981
jssimbine <jennyfersimbine@gmail.com>
2022-12-23 12:16:27 -0800

    fixed task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,23 +11,13 @@
 
 void reverse_array(int *a, int n)
 {
-
 	int temp;
 	int i;
-/**
- *	i = 0;
- *	while (i < n / 2)
- *	{
- *		temp = a[i];
- *		a[i] = a[n-i-1];
- *		a[n-i-1] = temp;
- *		i++;
- *	}
- */
- 	for(i = 0; i < n / 2; i++)
+
+	for (i = 0; i < n / 2; i++)
 	{
 		temp = a[i];
-		a[i] = a[n-i-1];
- 		a[n-i-1] = temp;
-	} 
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = temp;
+	}
 }
