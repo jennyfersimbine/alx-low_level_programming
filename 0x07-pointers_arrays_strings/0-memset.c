6797a3b0468b13826c738bcd4f2ce932d041f2d8
jssimbine <jennyfersimbine@gmail.com>
2023-01-03 18:18:48 -0800

    struggling

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * memset - fucntion that fills memory with a constant byte.
+ * @s: points the memory are
+ * @b: constant bytes to fill
+ * @n: n bytes of memory area
+ * Return:  s
+ */
+
+void *memset(void *s, char b, size_t n);
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+first project of 2023; dir for more on fuctions , pointers and arrays
