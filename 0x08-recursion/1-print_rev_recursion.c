8a5765875df2896ff0983d9b51e3a45bf349f73d
jssimbine <jennyfersimbine@gmail.com>
2023-01-05 14:33:43 -0800

    task 1 fixx

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,34 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _print_rev_recursion - print string in reverse with recursion
+ * @s: string to print
+ */
+
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
+	int i;
+	i = '\0';
 	/**
 	 *if (s[i] != '\0')
 	 *{
 	 *	i++;
 	 *}
 
+	 
+	 *	i -= 1;
+	 *	if (i >= 0)
+	 *{
+	 *	_putchar(s[i]);
+	 *	i--;
+	 *	_print_rev_recursion(s);
+	 *}
 	 */
-	i -= 1;
-	if (i >= 0)
+	i--;
+	_putchar(s[i]);
+	if (s[i] > 0)
 	{
-		_putchar(s[i]);
-		i--;
 		_print_rev_recursion(s);
 	}
 }
