2d7d74b0429a17cf4577e81e3f8f72da71869167
jssimbine <jennyfersimbine@gmail.com>
2023-01-05 10:09:33 -0800

    task o fixx

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,19 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\n' && *s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
-	} else
-	{
-		_putchar(*s);
-		_puts_recursion(s);
 	}
-	/**
-	 *else
-	 *{
-	 *	_putchar('\n');
-	 *}
-	 */
+	_putchar(*s);
+	_puts_recursion(s);
+	}
 }
