c6c719e1cc80097c72c36ca7e0aaea4ae323e778
jssimbine <jennyfersimbine@gmail.com>
2023-01-05 16:02:56 -0800

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+int _strlen_recursion(char *s)
+{
+	int result;
+
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	result = 1 + _strlen_recursion(s + 1);
+	return (result);
+}
