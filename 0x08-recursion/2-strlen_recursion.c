b91ae9c6e2ccfaec0765ede0486f3234b3f500bb
jssimbine <jennyfersimbine@gmail.com>
2023-01-05 16:09:13 -0800

    fix task 2 betty

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,13 +1,19 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * _strlen_recursion - return length of string (factorial)
+ * @s: string to count
+ * Return: result of count or 0
+ */
+
 int _strlen_recursion(char *s)
 {
 	int result;
 
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	result = 1 + _strlen_recursion(s + 1);
 	return (result);
