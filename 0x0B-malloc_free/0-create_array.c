42ecc545bacbfaad3422db9a63c66b6331ad1bae
jssimbine <jennyfersimbine@gmail.com>
2023-01-10 16:02:56 -0800

    mm task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include "_putchar.c"
+
 /**
  * create_array - create array of chars and initialize with specific char
  * @size: size of array
@@ -17,13 +17,12 @@ char *create_array(unsigned int size, char c)
 	{return (NULL);
 
 	}
-
 	arc = malloc(size * sizeof(char));
 	arc[0] = c;
 	i = 0;
 	while (i < size)
 	{
-		_putchar(arc[i]);
+		putchar(arc[i]);
 	}
 	if (arc == NULL)
 	{
