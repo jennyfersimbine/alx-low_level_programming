d10599b6a3fd4d7b28152ca950f70462e29b5f51
jennyfersimbine <jennyfersimbine38@gmail.com>
2023-12-15 12:52:09 -0800

    bit task 0,1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,27 @@
 #include "main.h"
+
+/**
+ * _pow - returns the value of x raised to the power of y
+ * @x: value
+ * @y: power value
+ * Return: result
+ */
+
+int _pow(int x, int y)
+{
+	int n;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+	{
+		n = 1;
+		return (n);
+	}
+	n = x * _pow(x, y - 1);
+	return (n);
+}
+
 /**
  * binary_to_uint - converts a binary number to an unsigned int
  * @b:  is pointing to a string of 0 and 1 chars
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,30 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * print_binary - prints the binary representation of a number
  * @n: number to represent
  */
 
+#include "main.h"
+
+int remain(int nr)
+{
+	int binDiv = 2;
+	while (nr >= binDiv)
+		nr -= binDiv;
+	return (nr);
+}
+
+void print_binary(unsigned long int n)
+{
+char *bstr = "";
+
+while (n > 0)
+{
+remainder = remain(n);
+/*register each bit or concatenate in string*/
+bstr = strcat(bstr, remainder);
+n = n >> 1;
+}
+printf("%s", bstr);
+}
